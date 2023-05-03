7fe9fcfb5215864904023d5b9fb9a710791cf986
pepenotti <antonio.franchinotti@gmail.com>
2023-05-03 00:04:18 -0300

    Remove back to top arrow

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ function scheduleTurn() {
         enableAll();
     })
     .fail(() => {
-        alert("error");
+        alert("Hubo un error en el sistema, por favor envíe un mail a consultas@dto.com.ar.");
     });
 }
 
@@ -49,24 +49,4 @@ function enableAll() {
     turnEmail.disabled = false;
     turnMessage.disabled = false;
     turnSubmit.innerHTML = "Solicitar turno"
-}
-
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
